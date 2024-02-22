a07c28c7fc5948a71afede70e76e45586f1280bc
Mondongo03 <rjulian@elpuig.xeill.net>
2024-02-22 20:20:14 +0100

    tgrghtr

--- android/src/com/ruben/game/AndroidLauncher.java
+++ android/src/com/ruben/game/AndroidLauncher.java
@@ -21,37 +21,13 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-
 		RelativeLayout layout = new RelativeLayout(this);
-
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-
 		View gameView = initializeForView(new Juego(), config);
-
-
-
-		// Add the gameView to the view hierarchy
 		layout.addView(gameView);
 
-
-
-		// Add the AdMob view
-		RelativeLayout.LayoutParams adParams =
-				new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-						RelativeLayout.LayoutParams.WRAP_CONTENT);
-		adParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-		adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-
-		// Add the AdView to the view hierarchy.
-
-		// Hook it all up
 		setContentView(layout);
-		
+
 	}
 
 
--- core/src/com/ruben/game/Juego.java
+++ core/src/com/ruben/game/Juego.java
@@ -2,7 +2,6 @@ package com.ruben.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
@@ -12,7 +11,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
@@ -29,32 +27,33 @@ import java.util.Iterator;
 
 public class Juego extends ApplicationAdapter {
 	private Texture background,background_2,background_3,background_4,bg,texture;
-	private Texture sacoImage;
 	private Sound coinSound;
 	private Sound bombSound;
-
+	private Texture sacoTexture;
+	private Texture muerteTexture;
 	private Texture bombTexture;
 	private Texture coinTexture;
-	private Texture missileTexture;
+	private Texture missilTexture;
 	private Texture starTexture;
 
 	private Music backgroundMusic;
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
 	private Rectangle saco;
-	private Array<Rectangle> bombdrops;
-	private Array<Rectangle> bombs;
+	private Array<Rectangle> monedas;
+	private Array<Rectangle> bombas;
+	private Array<Rectangle> muertes;
 	private Array<Rectangle> missiles;
-	private Array<Rectangle> stars;
+	private Array<Rectangle> lingotes;
 
-	private long lastDropTime;
-	private long lastDropTime2;
-	private long lastDropTime3;
-	private long lastDropTime4;
+	private long lastDropTimeCoin;
+	private long lastDropTimeBomba;
+	private long lastDropTimeMisil;
+	private long lastDropTimeLingote;
 
 	private float stateTime;
 	private int score,level;
-	private String yourScoreName,Leveltxt;
+	private String yourScoreName,Leveltxt, textoVacio;
 	private BitmapFont yourBitmapFontName;
 
 	float currentBgX;
@@ -63,11 +62,10 @@ public class Juego extends ApplicationAdapter {
 	private Stage stage;
 	private TextButton button;
 	private TextButton.TextButtonStyle textButtonStyle;
-
 	private Skin skin;
-
-	Label ScoreLabel,LevelLabel,HiLabel;
+	Label scoreLabel,LevelLabel,HiLabel;
 	public boolean paused = false;
+	private long lastDropTimeMuerte;
 
 	@Override
 	public void create() {
@@ -84,53 +82,41 @@ public class Juego extends ApplicationAdapter {
 		button.setScale(2,2);
 		stage.addActor(button);
 
-
-
 		currentBgX = 800;
 		lastTimeBg = TimeUtils.nanoTime();
 
-
-		sacoImage = new Texture(Gdx.files.internal("saco_monedas.png"));
-		background = new Texture(Gdx.files.internal("background.png"));
-		background_2 = new Texture(Gdx.files.internal("background_2.png"));
-		background_3 = new Texture(Gdx.files.internal("background_3.png"));
+		sacoTexture = new Texture(Gdx.files.internal("saco_monedas.png"));
+		background = new Texture(Gdx.files.internal("images.jpg"));
+		background_2 = new Texture(Gdx.files.internal("background.jpg"));
+		background_3 = new Texture(Gdx.files.internal("background3.png"));
 		background_4 = new Texture(Gdx.files.internal("background_4.png"));
-
 
 		bombSound = Gdx.audio.newSound(Gdx.files.internal("bomb.mp3"));
 		coinSound = Gdx.audio.newSound(Gdx.files.internal("coin.mp3"));
 		backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("bg.mp3"));
-
 		bombTexture = new Texture(Gdx.files.internal("bomb.png"));
-
 		coinTexture = new Texture(Gdx.files.internal("coin.png"));
-
-
-		missileTexture = new Texture(Gdx.files.internal("missile.png"));
+		muerteTexture = new Texture(Gdx.files.internal("skull.png"));
+		missilTexture = new Texture(Gdx.files.internal("missile.png"));
 
 		starTexture = new Texture(Gdx.files.internal("star.png"));
 
 		Gdx.graphics.setContinuousRendering(false);
 		Gdx.graphics.requestRendering();
 
+		texture = new Texture(Gdx.files.internal("background.jpg"));
 
-
-
-
-
-		texture = new Texture(Gdx.files.internal("background.png"));
-		texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
 		level=1;
-		yourScoreName = "Score:"+score;
+		yourScoreName = "Puntuación: "+score;
 		yourBitmapFontName = new BitmapFont();
 		Leveltxt="Level:"+level;
 
-		ScoreLabel = new Label(yourScoreName,new Label.LabelStyle(font12, Color.RED));
-		ScoreLabel.setPosition(Gdx.graphics.getWidth()/2,20);
-		ScoreLabel.setFontScale(2);
-		stage.addActor(ScoreLabel);
+		scoreLabel = new Label("", new Label.LabelStyle(font12, Color.RED));
+		scoreLabel.setPosition(Gdx.graphics.getWidth() / 2, 20);
+		scoreLabel.setFontScale(2);
+		stage.addActor(scoreLabel);
 
-		LevelLabel = new Label(Leveltxt,new Label.LabelStyle(font12, Color.BLACK));
+		LevelLabel = new Label(Leveltxt,new Label.LabelStyle(font12, Color.RED));
 		LevelLabel.setPosition(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()-40);
 		LevelLabel.setFontScale(2);
 		stage.addActor( LevelLabel);
@@ -139,15 +125,10 @@ public class Juego extends ApplicationAdapter {
 		int hiscore=  pref.getInteger("hiscore", 0);
 		String hiscoretxt="Hiscore:"+hiscore;
 
-		HiLabel = new Label(hiscoretxt,new Label.LabelStyle(font12, Color.BLACK));
+		HiLabel = new Label(hiscoretxt,new Label.LabelStyle(font12, Color.RED));
 		HiLabel.setPosition(Gdx.graphics.getWidth()*3/4,Gdx.graphics.getHeight()-40);
 		HiLabel.setFontScale(2);
 		stage.addActor( HiLabel);
-
-
-
-
-
 
 		backgroundMusic.setLooping(true);
 		backgroundMusic.play();
@@ -162,22 +143,18 @@ public class Juego extends ApplicationAdapter {
 		saco.width = 64;
 		saco.height = 64;
 
-		bombdrops = new Array<Rectangle>();
+		monedas = new Array<Rectangle>();
 		spawnCoin();
-
-		bombs = new Array<Rectangle>();
+		bombas = new Array<Rectangle>();
 		spawnBomb();
 		missiles = new Array<Rectangle>();
 		if(level>1)spawnMissile();
-
-		stars = new Array<Rectangle>();
-		if(level>3)spawnStar();
-
-
+		lingotes = new Array<Rectangle>();
+		if(level>3) spawnLingote();
+		muertes = new Array<Rectangle>();
 
 
 	}
-
 
 	private void spawnCoin() {
 		Rectangle bombdrop = new Rectangle();
@@ -185,8 +162,8 @@ public class Juego extends ApplicationAdapter {
 		bombdrop.y = 480;
 		bombdrop.width = 45;
 		bombdrop.height = 45;
-		bombdrops.add(bombdrop);
-		lastDropTime = TimeUtils.nanoTime();
+		monedas.add(bombdrop);
+		lastDropTimeCoin = TimeUtils.nanoTime();
 	}
 
 	private void spawnBomb() {
@@ -195,8 +172,8 @@ public class Juego extends ApplicationAdapter {
 		bomb.y = 480;
 		bomb.width = 64;
 		bomb.height = 64;
-		bombs.add(bomb);
-		lastDropTime2 = TimeUtils.nanoTime();
+		bombas.add(bomb);
+		lastDropTimeBomba = TimeUtils.nanoTime();
 	}
 	private void spawnMissile() {
 		Rectangle missile = new Rectangle();
@@ -205,19 +182,27 @@ public class Juego extends ApplicationAdapter {
 		missile.width = 64;
 		missile.height = 64;
 		missiles.add(missile);
-		lastDropTime3 = TimeUtils.nanoTime();
+		lastDropTimeMisil = TimeUtils.nanoTime();
+	}
+	private void spawnMuertes() {
+		Rectangle muerte = new Rectangle();
+		muerte.x = MathUtils.random(0, 800 - 64);
+		muerte.y = 480;
+		muerte.width = 45;
+		muerte.height = 45;
+		muertes.add(muerte);
+		lastDropTimeMuerte = TimeUtils.nanoTime();
 	}
 
-	private void spawnStar() {
-		Rectangle star = new Rectangle();
-		star.x = MathUtils.random(0, 800 - 64);
-		star.y = 480;
-		star.width = 70;
-		star.height = 70;
-		stars.add(star);
-		lastDropTime4 = TimeUtils.nanoTime();
+	private void spawnLingote() {
+		Rectangle lingote = new Rectangle();
+		lingote.x = MathUtils.random(0, 800 - 64);
+		lingote.y = 480;
+		lingote.width = 70;
+		lingote.height = 70;
+		lingotes.add(lingote);
+		lastDropTimeLingote = TimeUtils.nanoTime();
 	}
-
 
 	@Override
 	public void render() {
@@ -227,10 +212,10 @@ public class Juego extends ApplicationAdapter {
 
 		button.addListener(new ChangeListener() {
 			@Override
-			public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+			public void changed(ChangeEvent event, Actor actor) {
 				score=0;
 				yourScoreName = "Puntuación: " + score;
-				ScoreLabel.setText(yourScoreName);
+				scoreLabel.setText(yourScoreName);
 				Preferences prefs = Gdx.app.getPreferences("CoinHuntPreferences");
 				prefs.putInteger("Puntuación ", score);
 				prefs.flush();
@@ -244,6 +229,9 @@ public class Juego extends ApplicationAdapter {
 
 		});
 
+		scoreLabel.setText("Puntuación: " + score);
+
+
 		Preferences pref = Gdx.app.getPreferences("CoinHuntHi");
 		if(score> pref.getInteger("Puntuación Máxima", 0)){
 			pref.putInteger("Puntuación Máxima", score);
@@ -255,7 +243,6 @@ public class Juego extends ApplicationAdapter {
 
 		stateTime += Gdx.graphics.getDeltaTime();
 
-
 		camera.update();
 
 		if(level==1)bg=background;
@@ -263,45 +250,32 @@ public class Juego extends ApplicationAdapter {
 		if(level==3)bg=background_3;
 		if(level>3)bg=background_4;
 
-
-
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 
 		batch.draw(bg, currentBgX - 800, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.draw(bg, currentBgX, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
+		batch.draw(sacoTexture, saco.x, saco.y);
 
-		if(TimeUtils.nanoTime() - lastTimeBg > 100000000){
-			currentBgX -= 50;
-			lastTimeBg = TimeUtils.nanoTime();
-		}
-		if(currentBgX == 0){
-			currentBgX = 800;
-		}
-
-
-
-		batch.draw(sacoImage, saco.x, saco.y);
-
-		for (Rectangle bomb : bombs) {
+		for (Rectangle bomb : bombas) {
 			batch.draw(bombTexture, bomb.x, bomb.y, bomb.width, bomb.height);
 		}
-		for (Rectangle bombdrop : bombdrops) {
+		for (Rectangle bombdrop : monedas) {
 			batch.draw(coinTexture, bombdrop.x, bombdrop.y, bombdrop.width, bombdrop.height);
 		}
 		for (Rectangle missile : missiles) {
-			batch.draw(missileTexture, missile.x, missile.y, missile.width, missile.height);
+			batch.draw(missilTexture, missile.x, missile.y, missile.width, missile.height);
 		}
-		for (Rectangle star : stars) {
-			batch.draw(starTexture, star.x, star.y, star.width, star.height);
+		for (Rectangle lingote : lingotes) {
+			batch.draw(starTexture, lingote.x, lingote.y, lingote.width, lingote.height);
 		}
+		for (Rectangle muerte : muertes) {
+			batch.draw(muerteTexture, muerte.x, muerte.y, muerte.width, muerte.height);
+		}
+		textoVacio = "";
 
-
-
-
-
-		yourBitmapFontName.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+		yourBitmapFontName.setColor(0f, 0f, 0f, 0f);
 		yourBitmapFontName.draw(batch, yourScoreName, 25, 100);
 		yourBitmapFontName.getData().setScale(2, 2);
 
@@ -314,33 +288,31 @@ public class Juego extends ApplicationAdapter {
 			Vector3 touchPos = new Vector3();
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			saco.x = touchPos.x - 64 / 2;
+			saco.x = touchPos.x -30;
 		}
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) saco.x -= 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) saco.x += 200 * Gdx.graphics.getDeltaTime();
 
 
-		if (saco.x < 0) saco.x = 0;
-		if (saco.x > 800 - 64) saco.x = 800 - 64;
-
-		if (TimeUtils.nanoTime() - lastDropTime > 700000000) spawnCoin();
-		if (TimeUtils.nanoTime() - lastDropTime2 > 1500000000) spawnBomb();
-		if ((TimeUtils.nanoTime() - lastDropTime3 > 1000000000)&&level>1) spawnMissile();
-		if ((TimeUtils.nanoTime() - lastDropTime3 > 500000000)&&level>2) spawnMissile();
-		if ((TimeUtils.nanoTime() - lastDropTime3 > 500000000)&&level>3) spawnBomb();
-		if ((TimeUtils.nanoTime() - lastDropTime > 500000000)&&level>3) spawnCoin();
-		if ((TimeUtils.nanoTime() - lastDropTime > 350000000)&&level>7) spawnCoin();
-		if ((TimeUtils.nanoTime() - lastDropTime4 > 1500000000)&&level>3) spawnStar();
 
 
-		for (Iterator<Rectangle> iter = bombdrops.iterator(); iter.hasNext(); ) {
-			Rectangle bombdrop = iter.next();
-			bombdrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if (bombdrop.y + 64 < 0) iter.remove();
-			if (bombdrop.overlaps(saco)) {
+		if (TimeUtils.nanoTime() - lastDropTimeCoin > 700000000) spawnCoin();
+		if (TimeUtils.nanoTime() - lastDropTimeBomba > 1500000000) spawnBomb();
+		if (TimeUtils.nanoTime() - lastDropTimeMuerte > 6000000000L) spawnMuertes();
+		if ((TimeUtils.nanoTime() - lastDropTimeMisil > 1000000000)&&level>1) spawnMissile();
+		if ((TimeUtils.nanoTime() - lastDropTimeMisil > 500000000)&&level>2) spawnMissile();
+		if ((TimeUtils.nanoTime() - lastDropTimeMisil > 500000000)&&level>3) spawnBomb();
+		if ((TimeUtils.nanoTime() - lastDropTimeCoin > 500000000)&&level>3) spawnCoin();
+		if ((TimeUtils.nanoTime() - lastDropTimeCoin > 350000000)&&level>7) spawnCoin();
+		if ((TimeUtils.nanoTime() - lastDropTimeLingote > 1500000000)&&level>3) spawnLingote();
+
+
+		for (Iterator<Rectangle> iter = monedas.iterator(); iter.hasNext(); ) {
+			Rectangle coin = iter.next();
+			coin.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (coin.y + 64 < 0) iter.remove();
+			if (coin.overlaps(saco)) {
 				score++;
 				yourScoreName = "Puntuación: " + score;
-				ScoreLabel.setText(yourScoreName);
+				scoreLabel.setText(yourScoreName);
 
 				coinSound.play();
 				Preferences prefs = Gdx.app.getPreferences("CoinHuntPreferences");
@@ -350,7 +322,7 @@ public class Juego extends ApplicationAdapter {
 			}
 		}
 
-		for (Iterator<Rectangle> iter = bombs.iterator(); iter.hasNext(); ) {
+		for (Iterator<Rectangle> iter = bombas.iterator(); iter.hasNext(); ) {
 			Rectangle bomb = iter.next();
 			bomb.y -= 200 * Gdx.graphics.getDeltaTime();
 			if (bomb.y + 64 < 0) iter.remove();
@@ -358,7 +330,7 @@ public class Juego extends ApplicationAdapter {
 				score -= 5;
 				if (score < 0) score = 0;
 				yourScoreName = "Score:" + score;
-				ScoreLabel.setText(yourScoreName);
+				scoreLabel.setText(yourScoreName);
 
 				boom(2);
 				Preferences prefs = Gdx.app.getPreferences("CoinHuntPreferences");
@@ -376,7 +348,7 @@ public class Juego extends ApplicationAdapter {
 				score -=20;
 				if (score < 0) score = 0;
 				yourScoreName = "Score:" + score;
-				ScoreLabel.setText(yourScoreName);
+				scoreLabel.setText(yourScoreName);
 
 				boom(3);
 				Preferences prefs = Gdx.app.getPreferences("CoinHuntPreferences");
@@ -385,15 +357,31 @@ public class Juego extends ApplicationAdapter {
 				iter.remove();
 			}
 		}
+		for (Iterator<Rectangle> iter = muertes.iterator(); iter.hasNext(); ) {
+			Rectangle muerte = iter.next();
+			muerte.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (muerte.y + 64 < 0) iter.remove();
+			if (muerte.overlaps(saco)) {
+				score = 0;
+				yourScoreName = "Puntuación: " + score;
+				scoreLabel.setText(yourScoreName);
 
-		for (Iterator<Rectangle> iter = stars.iterator(); iter.hasNext(); ) {
+				coinSound.play();
+				Preferences prefs = Gdx.app.getPreferences("CoinHuntPreferences");
+				prefs.putInteger("Puntuación: ", score);
+				prefs.flush();
+				iter.remove();
+			}
+		}
+
+		for (Iterator<Rectangle> iter = lingotes.iterator(); iter.hasNext(); ) {
 			Rectangle star = iter.next();
 			star.y -= 400 * Gdx.graphics.getDeltaTime();
 			if (star.y + 64 < 0) iter.remove();
 			if (star.overlaps(saco)) {
 				score+=10;
 				yourScoreName = "Score:" + score;
-				ScoreLabel.setText(yourScoreName);
+				scoreLabel.setText(yourScoreName);
 
 				coinSound.play();
 				Preferences prefs = Gdx.app.getPreferences("CoinHuntPreferences");
@@ -403,8 +391,6 @@ public class Juego extends ApplicationAdapter {
 			}
 		}
 
-
-
 		level=getgamelevel(score) ;
 		LevelLabel.setText("Level:"+level);
 
@@ -413,37 +399,28 @@ public class Juego extends ApplicationAdapter {
 		}
 
 	}
-	private void boom(int intensity)
-	{
+	private void boom(int intensity){
 		bombSound.play();
-
 	}
 
 	private int getgamelevel(int score)
 	{
 		int i = 1;
-		if (score >=10){
-			i=2;
-		}
-		if (score >=20){
-			i=3;
-		}
-		if (score >=50){
-			i=4;
-		}
-		if (score >=100){
-			i=5;
-		}
+		if (score >=10) i=2;
+
+		if (score >=20) i=3;
+
+		if (score >=25) i=4;
+
+		if (score >=27)i=5;
+
 		return i;
 	}
 
-
-
 	@Override
 	public void dispose() {
-
 		background.dispose();
-		sacoImage.dispose();
+		sacoTexture.dispose();
 		coinSound.dispose();
 		bombSound.dispose();
 		backgroundMusic.dispose();
